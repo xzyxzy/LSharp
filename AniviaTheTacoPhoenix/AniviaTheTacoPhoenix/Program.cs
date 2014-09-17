51af1e8a71eed4bad59db80f9a455a112e24225f
xthanhz <zthanhz251@gmail.coms>
2014-09-17 13:36:49 -0500

    Anivia Release

---
+++ AniviaTheTacoPhoenix/AniviaTheTacoPhoenix/Program.cs
@@ -0,0 +1,354 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+using Color = System.Drawing.Color;
+
+
+namespace AniviaTheTacoPhoenix
+{
+    class Program
+    {
+        public const string ChampionName = "Anivia";
+
+        //Orbwalker instance
+        public static Orbwalking.Orbwalker Orbwalker;
+
+        //Spells
+        public static List<Spell> SpellList = new List<Spell>();
+
+        public static Spell Q;
+        public static Obj_SpellMissile qpos;
+        public static bool qcreated = false;
+        public static Spell W;
+        public static Spell E;
+        public static Spell R;
+        public static Vector2 rpos;
+        public static bool rcreated = false;
+        public static bool firstCreate = false;
+        public static bool rFarm = false;
+        public static float[] wWidth = { 400f, 500f, 600f, 700f, 800f };
+
+        public static SpellSlot IgniteSlot;
+
+        //Menu
+        public static Menu menu;
+
+        private static Obj_AI_Hero Player;
+        private static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            Player = ObjectManager.Player;
+
+            //check to see if correct champ
+            if (Player.BaseSkinName != ChampionName) return;
+
+            //intalize spell
+            Q = new Spell(SpellSlot.Q, 1100);
+            W = new Spell(SpellSlot.W, 1000);
+            E = new Spell(SpellSlot.E, 650);
+            R = new Spell(SpellSlot.R, 625);
+
+            IgniteSlot = Player.GetSpellSlot("SummonerDot");
+
+            Q.SetSkillshot(.25f, 110f, 850f, false, SkillshotType.SkillshotLine);
+            W.SetSkillshot(.25f, 400f, float.MaxValue, false, SkillshotType.SkillshotLine);
+            R.SetSkillshot(.25f, 400f, float.MaxValue, false, SkillshotType.SkillshotCircle);
+
+            SpellList.Add(Q);
+            SpellList.Add(W);
+            SpellList.Add(E);
+            SpellList.Add(R);
+
+            menu = new Menu(ChampionName, ChampionName, true);
+
+            //Orbwalker submenu
+            menu.AddSubMenu(new Menu("Orbwalking", "Orbwalking"));
+
+            //Target selector
+            var targetSelectorMenu = new Menu("Target Selector", "Target Selector");
+            SimpleTs.AddToMenu(targetSelectorMenu);
+            menu.AddSubMenu(targetSelectorMenu);
+
+            //Orbwalk
+            Orbwalker = new Orbwalking.Orbwalker(menu.SubMenu("Orbwalking"));
+
+            //Combo menu:
+            menu.AddSubMenu(new Menu("Combo", "Combo"));
+            menu.SubMenu("Combo").AddItem(new MenuItem("UseQCombo", "Use Q").SetValue(true));
+            menu.SubMenu("Combo").AddItem(new MenuItem("UseWCombo", "Use W").SetValue(true));
+            menu.SubMenu("Combo").AddItem(new MenuItem("UseECombo", "Use E").SetValue(true));
+            menu.SubMenu("Combo").AddItem(new MenuItem("UseRCombo", "Use R").SetValue(true));
+            menu.SubMenu("Combo").AddItem(new MenuItem("UseIgniteCombo", "Use Ignite").SetValue(true));
+            menu.SubMenu("Combo")
+                .AddItem(
+                    new MenuItem("ComboActive", "Combo!").SetValue(
+                        new KeyBind(menu.Item("Orbwalk").GetValue<KeyBind>().Key, KeyBindType.Press)));
+
+            //Harass menu:
+            menu.AddSubMenu(new Menu("Harass", "Harass"));
+            menu.SubMenu("Harass").AddItem(new MenuItem("UseQHarass", "Use Q").SetValue(true));
+            menu.SubMenu("Harass").AddItem(new MenuItem("UseWHarass", "Use W").SetValue(false));
+            menu.SubMenu("Harass").AddItem(new MenuItem("UseEHarass", "Use E").SetValue(true));
+            menu.SubMenu("Harass")
+                .AddItem(
+                    new MenuItem("HarassActive", "Harass!").SetValue(
+                        new KeyBind(menu.Item("Farm").GetValue<KeyBind>().Key, KeyBindType.Press)));
+            menu.SubMenu("Harass")
+                .AddItem(
+                    new MenuItem("HarassActiveT", "Harass (toggle)!").SetValue(new KeyBind("Y".ToCharArray()[0],
+                        KeyBindType.Toggle)));
+
+            //Farming menu:
+            menu.AddSubMenu(new Menu("Farm", "Farm"));
+            menu.SubMenu("Farm").AddItem(new MenuItem("UseQFarm", "Use Q").SetValue(false));
+            menu.SubMenu("Farm").AddItem(new MenuItem("UseEFarm", "Use E").SetValue(false));
+            menu.SubMenu("Farm").AddItem(new MenuItem("LaneClearActive", "Farm!").SetValue(new KeyBind(menu.Item("LaneClear").GetValue<KeyBind>().Key, KeyBindType.Press)));
+
+            //Drawings menu:
+            menu.AddSubMenu(new Menu("Drawings", "Drawings"));
+            menu.SubMenu("Drawings")
+                .AddItem(new MenuItem("QRange", "Q range").SetValue(new Circle(false, Color.FromArgb(100, 255, 0, 255))));
+            menu.SubMenu("Drawings")
+                .AddItem(new MenuItem("WRange", "W range").SetValue(new Circle(true, Color.FromArgb(100, 255, 0, 255))));
+            menu.SubMenu("Drawings")
+                .AddItem(new MenuItem("ERange", "E range").SetValue(new Circle(false, Color.FromArgb(100, 255, 0, 255))));
+            menu.SubMenu("Drawings")
+                .AddItem(new MenuItem("RRange", "R range").SetValue(new Circle(false, Color.FromArgb(100, 255, 0, 255))));
+            menu.AddToMainMenu();
+
+            //Events
+            Game.OnGameUpdate += Game_OnGameUpdate;
+            Drawing.OnDraw += Drawing_OnDraw;
+            AntiGapcloser.OnEnemyGapcloser += AntiGapcloser_OnEnemyGapcloser;
+            Interrupter.OnPosibleToInterrupt += Interrupter_OnPosibleToInterrupt;
+            GameObject.OnCreate += OnCreate;
+            GameObject.OnDelete += OnDelete;
+            Obj_AI_Base.OnProcessSpellCast += Obj_AI_Base_OnProcessSpellCast;
+            Game.PrintChat(ChampionName + " Loaded! --- By xSalice");
+        }
+
+        private static float GetComboDamage(Obj_AI_Base enemy)
+        {
+            var damage = 0d;
+
+            if (Q.IsReady())
+                damage += DamageLib.getDmg(enemy, DamageLib.SpellType.Q);
+            if (E.IsReady())
+                damage += DamageLib.getDmg(enemy, DamageLib.SpellType.E);
+            if (IgniteSlot != SpellSlot.Unknown && Player.SummonerSpellbook.CanUseSpell(IgniteSlot) == SpellState.Ready)
+                damage += DamageLib.getDmg(enemy, DamageLib.SpellType.IGNITE);
+            if (R.IsReady())
+                damage += DamageLib.getDmg(enemy, DamageLib.SpellType.R);
+
+            return (float)damage;
+        }
+
+        private static void UseSpells(bool useQ, bool useW, bool useE, bool useR, bool useIgnite)
+        {
+            var qTarget = SimpleTs.GetTarget(Q.Range, SimpleTs.DamageType.Magical);
+            var wTarget = SimpleTs.GetTarget(W.Range, SimpleTs.DamageType.Magical);
+            var eTarget = SimpleTs.GetTarget(E.Range, SimpleTs.DamageType.Magical);
+            var rTarget = SimpleTs.GetTarget(R.Range, SimpleTs.DamageType.Magical);
+
+            if (useW && wTarget != null && W.IsReady() && GetComboDamage(qTarget) >= qTarget.Health)
+            {
+                var pos = wTarget.ServerPosition;
+                var mypos = ObjectManager.Player.ServerPosition;
+                var newpos = pos + (pos - mypos) * (150 / ObjectManager.Player.Distance(wTarget));
+                W.Cast(newpos, true);
+                return;
+            }
+
+            if (useQ && qTarget != null && Q.IsReady() && Q.GetPrediction(qTarget).Hitchance >= HitChance.High && !qcreated)
+            {
+                Q.Cast(qTarget, true);
+                return;
+            }
+
+            if (useR && rTarget != null && R.IsReady() && !rcreated && Player.Distance(rTarget) <= R.Range)
+            {
+
+                var newpos2 = new Vector2(rTarget.ServerPosition.X, rTarget.ServerPosition.Y);
+                rpos = newpos2;
+                R.Cast(newpos2);
+                firstCreate = true;
+                return;
+            }
+
+            if (useE && eTarget != null && E.IsReady() && eTarget.HasBuff("Chilled"))
+            {
+                E.CastOnUnit(eTarget, true);
+            }
+
+        }
+
+        private static void Combo()
+        {
+            Orbwalker.SetAttacks(!(Q.IsReady()));
+            UseSpells(menu.Item("UseQCombo").GetValue<bool>(), menu.Item("UseWCombo").GetValue<bool>(),
+                menu.Item("UseECombo").GetValue<bool>(), menu.Item("UseRCombo").GetValue<bool>(),
+                menu.Item("UseIgniteCombo").GetValue<bool>());
+        }
+
+        private static void Harass()
+        {
+            UseSpells(menu.Item("UseQHarass").GetValue<bool>(), menu.Item("UseWHarass").GetValue<bool>(),
+                menu.Item("UseEHarass").GetValue<bool>(), false, false);
+        }
+
+        public static void detonateQ()
+        {
+            foreach (var enemy in ObjectManager.Get<Obj_AI_Hero>().Where(enemy => enemy.IsValidTarget()))
+            {
+                if (qpos != null && enemy.ServerPosition.Distance(qpos.Position) < 110 && enemy != null && qcreated)
+                {
+                    Q.Cast();
+                    return;
+                }
+            }
+        }
+
+        public static void checkR()
+        {
+            var potentialTargets = new List<Obj_AI_Base>();
+            foreach (var enemy in ObjectManager.Get<Obj_AI_Hero>().Where(enemy => enemy.IsValidTarget()))
+            {
+                if (rpos != null && !(enemy.ServerPosition.To2D().Distance(rpos) < R.Width - 50) && enemy != null && rcreated && R.IsReady())
+                {
+                    R.Cast(rpos);
+                    return;
+                }
+            }
+        }
+
+        private static void Farm()
+        {
+            if (!Orbwalking.CanMove(40)) return;
+
+            var allMinionsQ = MinionManager.GetMinions(ObjectManager.Player.ServerPosition, Q.Range, MinionTypes.All, MinionTeam.NotAlly);
+            var allMinionsE = MinionManager.GetMinions(ObjectManager.Player.ServerPosition, E.Range, MinionTypes.All, MinionTeam.NotAlly);
+
+            var useQ = menu.Item("UseQFarm").GetValue<bool>();
+            var useE = menu.Item("UseEFarm").GetValue<bool>();
+
+            if (useQ && Q.IsReady() && !qcreated)
+            {
+                var qPos = Q.GetLineFarmLocation(allMinionsQ);
+                if (qPos.MinionsHit >= 3)
+                {
+                    Q.Cast(qPos.Position, true);
+                }
+            }
+
+            if (useE && allMinionsE.Count > 0 && E.IsReady())
+                E.Cast(allMinionsE[0]);
+
+        }
+
+        private static void Game_OnGameUpdate(EventArgs args)
+        {
+            //check if player is dead
+            if (Player.IsDead) return;
+
+            detonateQ();
+
+            checkR();
+
+            Orbwalker.SetAttacks(true);
+
+            if (menu.Item("ComboActive").GetValue<KeyBind>().Active)
+            {
+                Combo();
+            }
+            else
+            {
+                if (menu.Item("HarassActive").GetValue<KeyBind>().Active ||
+                    menu.Item("HarassActiveT").GetValue<KeyBind>().Active)
+                    Harass();
+
+                if (menu.Item("LaneClearActive").GetValue<KeyBind>().Active)
+                    Farm();
+            }
+
+        }
+
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+            foreach (var spell in SpellList)
+            {
+                var menuItem = menu.Item(spell.Slot + "Range").GetValue<Circle>();
+                if (menuItem.Active)
+                    Utility.DrawCircle(Player.Position, spell.Range, menuItem.Color);
+            }
+        }
+
+        private static void OnCreate(GameObject sender, EventArgs args)
+        {
+            var spell = (Obj_SpellMissile)sender;
+            var unit = spell.SpellCaster.Name;
+            var name = spell.SData.Name;
+
+            if (unit == ObjectManager.Player.Name && name == "FlashFrostSpell")
+            {
+                qpos = spell;
+                qcreated = true;
+                return;
+            }
+        }
+
+        private static void OnDelete(GameObject sender, EventArgs args)
+        {
+            var spell = (Obj_SpellMissile)sender;
+            var unit = spell.SpellCaster.Name;
+            var name = spell.SData.Name;
+
+            if (unit == ObjectManager.Player.Name && name == "FlashFrostSpell")
+            {
+                qpos = null;
+                qcreated = false;
+                return;
+            }
+        }
+
+        public static void Obj_AI_Base_OnProcessSpellCast(Obj_AI_Base unit, GameObjectProcessSpellCastEventArgs attack)
+        {
+            if (attack.SData.Name == "GlacialStorm")
+            {
+                if (rcreated)
+                {
+                    rcreated = false;
+                    firstCreate = false;
+                }
+                else if (!rcreated && firstCreate)
+                {
+                    rcreated = true;
+                    firstCreate = false;
+                }
+
+            }
+        }
+
+        public static void AntiGapcloser_OnEnemyGapcloser(ActiveGapcloser gapcloser)
+        {
+            if (W.IsReady() && gapcloser.Sender.IsValidTarget(W.Range))
+                W.Cast(Player, true);
+        }
+
+        private static void Interrupter_OnPosibleToInterrupt(Obj_AI_Base unit, InterruptableSpell spell)
+        {
+            if (!menu.Item("InterruptSpells").GetValue<bool>()) return;
+
+            W.Cast(unit);
+        }
+    }
+}
