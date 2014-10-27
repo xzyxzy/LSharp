af76ea4cee7ff3f36f11f12a7da5e6aaa88efe8e
xSalice <xSalicez@gmail.com>
2014-10-27 12:39:54 -0500

    tweak

--- VeigarLittleEvil/VeigarLittleEvil/Program.cs
+++ VeigarLittleEvil/VeigarLittleEvil/Program.cs
@@ -441,7 +441,7 @@ namespace VeigarLittleEvil
             {
                 foreach (var minion in allMinions)
                 {
-                    if (minion.IsValidTarget() && HealthPrediction.GetHealthPrediction(minion, (int)(Player.Distance(minion) * 1000 / 1400)) < Damage.GetSpellDamage(Player, minion, SpellSlot.Q) - 20)
+                    if (minion.IsValidTarget() && HealthPrediction.GetHealthPrediction(minion, (int)(Player.Distance(minion) * 1000 / 1400)) < Damage.GetSpellDamage(Player, minion, SpellSlot.Q) - 25)
                     {
                         if (Q.IsReady())
                         {
