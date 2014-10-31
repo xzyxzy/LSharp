de3ce823b497fbf1b4fe0b5a99dd56706e301644
xSalice <xSalicez@gmail.com>
2014-10-30 20:54:51 -0500

    value

--- OriannaWreckingBalls/OriannaWreckingBalls/Program.cs
+++ OriannaWreckingBalls/OriannaWreckingBalls/Program.cs
@@ -56,7 +56,7 @@ namespace OriannaWreckingBalls
             E = new Spell(SpellSlot.E, 1095);
             R = new Spell(SpellSlot.R, 370);
 
-            Q.SetSkillshot(0.5f, 80, 1200, false, SkillshotType.SkillshotLine);
+            Q.SetSkillshot(0.25f, 80, 1300, false, SkillshotType.SkillshotLine);
             W.SetSkillshot(0f, 250, float.MaxValue, false, SkillshotType.SkillshotCircle);
             E.SetSkillshot(0.25f, 145, 1700, false, SkillshotType.SkillshotLine);
             R.SetSkillshot(0.60f, 370, float.MaxValue, false, SkillshotType.SkillshotCircle);
@@ -900,7 +900,7 @@ namespace OriannaWreckingBalls
                     if (ballStatus == 0)
                     {
                         Q.From = Player.ServerPosition;
-                        var pred = Q.GetCircularFarmLocation(allMinionsQ,Q.Width + 15);
+                        var pred = Q.GetCircularFarmLocation(allMinionsQ,Q.Width + 35);
 
                         if (pred.MinionsHit >= min)
                             Q.Cast(pred.Position, packets());
