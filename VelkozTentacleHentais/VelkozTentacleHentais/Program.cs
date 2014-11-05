6e86b02d845985dbf13d334ee1cb50e67e6d893d
xSalice <xSalicez@gmail.com>
2014-11-04 22:54:56 -0600

    rawr

--- VelkozTentacleHentais/VelkozTentacleHentais/Program.cs
+++ VelkozTentacleHentais/VelkozTentacleHentais/Program.cs
@@ -57,15 +57,15 @@ namespace VelkozTentacleHentais
 
             //intalize spell
             Q = new Spell(SpellSlot.Q, 1000);
-            QSplit = new Spell(SpellSlot.Q, 900);
+            QSplit = new Spell(SpellSlot.Q, 800);
             QDummy = new Spell(SpellSlot.Q, (float) Math.Sqrt(Math.Pow(Q.Range, 2) + Math.Pow(QSplit.Range, 2)));
             W = new Spell(SpellSlot.W, 900);
             E = new Spell(SpellSlot.E, 850);
             R = new Spell(SpellSlot.R, 1500);
 
-            Q.SetSkillshot(0.25f, 50f, 1300f, true, SkillshotType.SkillshotLine);
-            QDummy.SetSkillshot(0.25f, 55f, float.MaxValue, false, SkillshotType.SkillshotLine);
-            QSplit.SetSkillshot(0.25f, 55f, 2100, true, SkillshotType.SkillshotLine);
+            Q.SetSkillshot(0.25f, 60f, 1300f, true, SkillshotType.SkillshotLine);
+            QDummy.SetSkillshot(0.25f, 65f, float.MaxValue, false, SkillshotType.SkillshotLine);
+            QSplit.SetSkillshot(0.25f, 65f, 2100, true, SkillshotType.SkillshotLine);
             W.SetSkillshot(0.25f, 85f, 1700f, false, SkillshotType.SkillshotLine);
             E.SetSkillshot(0.5f, 80f, 1500f, false, SkillshotType.SkillshotCircle);
             R.SetSkillshot(0.3f, 1f, float.MaxValue, false, SkillshotType.SkillshotLine);
@@ -255,6 +255,7 @@ namespace VelkozTentacleHentais
                 W.GetPrediction(target).Hitchance >= HitChance.High)
             {
                 W.Cast(target);
+                return;
             }
 
             if (useE && target != null && E.IsReady() && Player.Distance(target) < E.Range &&
