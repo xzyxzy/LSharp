11761fb93ec9624e91cdc390daca2d4831e77e63
xSalice <xSalicez@gmail.com>
2014-11-29 22:51:50 -0600

    rawr

--- xSaliceReligionAIO/xSaliceReligionAIO/Champions/Syndra.cs
+++ xSaliceReligionAIO/xSaliceReligionAIO/Champions/Syndra.cs
@@ -433,7 +433,7 @@ namespace xSaliceReligionAIO.Champions
             _qe.Delay = Player.Distance(qeTarget)/E.Speed + Game.Ping/1000;
             
             var qePred = _qe.GetPrediction(qeTarget);
-            var predVec = Player.ServerPosition + Vector3.Normalize(qePred.UnitPosition - Player.ServerPosition) * (E.Range - 300);
+            var predVec = Player.ServerPosition + Vector3.Normalize(qePred.UnitPosition - Player.ServerPosition) * (E.Range - 350);
 
             if (!Q.IsReady() || !E.IsReady())
                 return;
