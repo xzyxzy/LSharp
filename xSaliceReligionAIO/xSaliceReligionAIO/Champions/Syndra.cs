dcee4b9660b1654954556644bbf866c2c6596607
xSalice <xSalicez@gmail.com>
2014-11-30 14:50:02 -0600

    rawr

--- xSaliceReligionAIO/xSaliceReligionAIO/Champions/Syndra.cs
+++ xSaliceReligionAIO/xSaliceReligionAIO/Champions/Syndra.cs
@@ -323,16 +323,6 @@ namespace xSaliceReligionAIO.Champions
                 {
                     W.UpdateSourcePosition(Get_Current_Orb().ServerPosition, Get_Current_Orb().ServerPosition);
 
-                    if (Player.Distance(wTarget) < E.Range - 100)
-                    {
-                        if (wToggleState != 1 && W.IsReady() &&
-                            Environment.TickCount - W.LastCastAttemptT > -300 + Game.Ping)
-                        {
-                            W.Cast(wTarget);
-                            return;
-                        }
-                    }
-
                     if (W.IsReady())
                     {
                         W.Cast(wTarget);
