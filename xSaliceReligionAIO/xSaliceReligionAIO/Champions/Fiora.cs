03927b28c36fc542852730429c04b8d5a0ef0882
xSalice <xSalicez@gmail.com>
2014-12-07 18:59:19 -0600

    rawr

--- xSaliceReligionAIO/xSaliceReligionAIO/Champions/Fiora.cs
+++ xSaliceReligionAIO/xSaliceReligionAIO/Champions/Fiora.cs
@@ -505,10 +505,10 @@ namespace xSaliceReligionAIO.Champions
                 }
             }
 
-            if (args.SData.Name.Contains("Attack") && args.Target.IsMe && Player.Distance(args.End) < 450)
+            if (xSLxOrbwalker.IsAutoAttack(args.SData.Name) && args.Target.IsMe && Player.Distance(args.End) < 450)
             {
                 if (menu.Item("W_Incoming").GetValue<bool>() ||
-                    (menu.Item("ComboActive").GetValue<KeyBind>().Active && E.IsReady() && menu.Item("UseWCombo").GetValue<bool>()) ||
+                    (menu.Item("ComboActive").GetValue<KeyBind>().Active && E.IsReady() &&menu.Item("UseWCombo").GetValue<bool>()) ||
                     (menu.Item("HarassActive").GetValue<KeyBind>().Active && menu.Item("UseWHarass").GetValue<bool>()))
                 {
                     if (!menu.Item("W_minion").GetValue<bool>())
