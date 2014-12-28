0e80c64c397cbb4bed2de00d628b78a9be18da32
xSalice <xSalicez@gmail.com>
2014-12-27 23:56:13 -0600

    rawr

--- ViktorTheMindBlower/ViktorTheMindBlower/Program.cs
+++ ViktorTheMindBlower/ViktorTheMindBlower/Program.cs
@@ -1,14 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using LeagueSharp;
 using LeagueSharp.Common;
 using SharpDX;
 using Color = System.Drawing.Color;
-using LX_Orbwalker;
 
 namespace ViktorTheMindBlower
 {
