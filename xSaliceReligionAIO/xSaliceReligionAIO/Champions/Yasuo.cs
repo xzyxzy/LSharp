d212429257ee421b563992fe1aab2712ed4c5b52
xSalice <xSalicez@gmail.com>
2015-01-05 05:15:23 -0600

    rawr

--- xSaliceReligionAIO/xSaliceReligionAIO/Champions/Yasuo.cs
+++ xSaliceReligionAIO/xSaliceReligionAIO/Champions/Yasuo.cs
@@ -632,7 +632,7 @@ namespace xSaliceReligionAIO.Champions
 
                 }
 
-                if (menu.Item(args.SData.Name + "W_Wall").GetValue<bool>() && W.IsReady() && Player.Distance(args.Start) < 500 && Player.Distance(args.End) < 500)
+                if (menu.Item(args.SData.Name + "W_Wall").GetValue<bool>() && W.IsReady() && (Player.Distance(args.Start) < 500 || Player.Distance(args.End) < 500))
                 {
                     W.Cast(args.Start, packets());
 
